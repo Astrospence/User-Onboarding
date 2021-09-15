d921a3ac98144f01abb0ee31c3fefcb1e61c7ede
Astrospence <illustrator.wood@gmail.com>
2021-09-15 13:37:32 -0600

    built rendering in Form.js

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,9 +1,67 @@
 import React from 'react';
 
 export default function Form(props) {
+    const { values, change, submit, disabled, errors } = props;
+
+    const onSubmit = evt => {
+
+    }
+
+    const onChange = evt => {
+
+    }
+
     return (
-        <div>
-            
-        </div>
+        <form className="form-container" onSubmit={onSubmit}>
+
+            <div className="form-section submit">
+                <h2>Create Your Gaming Profile</h2>
+                <button disabled={disabled}>Submit</button>
+                <div className="errors">
+                    <div>{errors.username}</div>
+                    <div>{errors.email}</div>
+                    <div>{errors.password}</div>
+                    <div>{errors.terms}</div>
+                </div>
+            </div>
+
+            <div className="form-section inputs">
+                <label>Username&nbsp;
+                    <input 
+                        type="text"
+                        name="username"
+                        value={values.username}
+                        onChange={onChange}
+                    />
+                </label>
+
+                <label>Email&nbsp;
+                    <input
+                        type="text"
+                        name="email"
+                        value={values.email}
+                        onChange={onChange}
+                    />
+                </label>
+
+                <label>Password&nbsp;
+                    <input
+                        type="text"
+                        name="password"
+                        value={values.password}
+                        onChange={onChange}
+                    />
+                </label>
+
+                <label>I read and agree to the Terms of Service&nbsp;
+                    <input
+                        type="checkbox"
+                        name="terms"
+                        checked={values.terms}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+        </form>
     )
 }
