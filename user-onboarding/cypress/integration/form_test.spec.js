5c50075f48f71095f4d6038082d303839f2d59f1
Astrospence <illustrator.wood@gmail.com>
2021-09-16 14:20:16 -0600

    created tests for navigation submitbutton and text inputs

--- user-onboarding/cypress/integration/form_test.spec.js
+++ user-onboarding/cypress/integration/form_test.spec.js
@@ -10,4 +10,33 @@ describe("User-Onboarding App", () => {
     const terms = () => cy.get('[data-cy=terms]');
     const submitButton = () => cy.get('[data-cy=submit]');
 
+    describe('Can the user input information', () => {
+        it('can navigate to the site', () => {
+            cy.url().should('include', 'localhost:3000');
+        })
+
+        it('submit button initializes as disabled', () => {
+            submitButton().should('be.disabled');
+        })
+
+        it('can type into the text inputs', () => {
+            usernameInput()
+                .should('have.value', '')
+                .type('Astrospence')
+                .should('have.value', 'Astrospence');
+
+            emailInput()
+                .should('have.value', '')
+                .type('mage@mage.com')
+                .should('have.value', 'mage@mage.com');
+
+            passwordInput()
+                .should('have.value', '')
+                .type('magesrock')
+                .should('have.value', 'magesrock');
+        })
+
+        
+    })
+
 })
