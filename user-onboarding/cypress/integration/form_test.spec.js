36214e91ac60741e9879b43178664f416ee10480
Astrospence <illustrator.wood@gmail.com>
2021-09-16 15:18:48 -0600

    user can submit form and it creates a new warrior

--- user-onboarding/cypress/integration/form_test.spec.js
+++ user-onboarding/cypress/integration/form_test.spec.js
@@ -2,7 +2,7 @@ describe("User-Onboarding App", () => {
     beforeEach(() => {
         cy.visit('localhost:3000');
     })
-
+    //helpers
     const usernameInput = () => cy.get('[data-cy=username]');
     const emailInput = () => cy.get('[data-cy=email]');
     const passwordInput = () => cy.get('[data-cy=password]');
@@ -19,32 +19,69 @@ describe("User-Onboarding App", () => {
             submitButton().should('be.disabled');
         })
 
-        it('can type into the text inputs', () => {
+        it('text inputs function', () => {
             usernameInput()
+                //first check that it initializes with no value
                 .should('have.value', '')
+                //then check functionality
                 .type('Astrospence')
                 .should('have.value', 'Astrospence');
 
             emailInput()
+                //first check that it initializes with no value
                 .should('have.value', '')
+                //then check functionality
                 .type('mage@mage.com')
                 .should('have.value', 'mage@mage.com');
 
             passwordInput()
+                //first check that it initializes with no value
                 .should('have.value', '')
+                //then check functionality
                 .type('magesrock')
                 .should('have.value', 'magesrock');
         })
 
-        it('can select a warrior class', () => {
+        it('warrior class selector functions', () => {
             warriorClass()
+                //first check that it initializes with no value
                 .should('have.value', '')
+                //then check that each selection will update the value
                 .select('Swordsman')
                 .select('Archer')
                 .select('Necromancer')
                 .select('Cleric')
-                .select('Mage');
-                
+                .select('Mage');    
+        })
+
+        it('terms of service checkbox functions', () => {
+            terms()
+                //first check that it initializes unchecked
+                .should('not.be.checked')
+                //then check functionality
+                .check()
+                .uncheck()
+                //check once more to enable submit button test
+                .check();
+        })
+    })
+
+    describe('User can submit form and new Warrior is created', () => {
+        it('submit button enables once all inputs have valid values', () => {
+            //simulate new entry
+            usernameInput().type('Astrospence');
+            emailInput().type('mage@mage.com');
+            passwordInput().type('magesrock');
+            warriorClass().select('Mage');
+            terms().check();
+            //check that the submit button enables once all values are in, then test functionality
+            submitButton()
+                .should('not.be.disabled')
+                .click();
+            //check that a new Warrior was added to the registry with username, email, and warrior class
+            cy.contains('Astrospence',);
+            cy.contains('mage@mage.com');
+            cy.contains('Mage');
         })
     })
 
--- user-onboarding/src/Warrior.js
+++ user-onboarding/src/Warrior.js
@@ -4,7 +4,7 @@ export default function Warrior(props) {
     const { values } = props;
     
     return(
-        <div className="warrior-container">
+        <div className="warrior-container" data-cy="warriorContainer">
             <h3>{values.username}</h3>
             <p>Class: {values.warriorClass}</p>
             <p>Rally to the Cause: {values.email}</p>
